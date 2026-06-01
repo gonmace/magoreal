51dac495bbf9fe8c909c3ba40c12b3ac9cf02d86
gonmave <gonmace@outlook.com>
2026-05-31 22:50:08 -0400

    chore: default langs en warm_translations → en pt de fr

--- landing/management/commands/warm_translations.py
+++ landing/management/commands/warm_translations.py
@@ -29,9 +29,9 @@ class Command(BaseCommand):
         parser.add_argument(
             '--langs',
             nargs='+',
-            default=['en', 'pt'],
+            default=['en', 'pt', 'de', 'fr'],
             metavar='LANG',
-            help='Idiomas a traducir (default: en pt)',
+            help='Idiomas a traducir (default: en pt de fr)',
         )
         parser.add_argument(
             '--clear',
